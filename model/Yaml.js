f4241829af01b21c7d62257732522f754dd8d7fd
七七 <12011613+fengyeplugin@user.noreply.gitee.com>
2023-01-09 07:25:19 +0000

    花生
    Signed-off-by: 七七 <12011613+fengyeplugin@user.noreply.gitee.com>

---
+++ model/Yaml.js
@@ -0,0 +1,38 @@
+﻿import plugin from'../../../lib/plugins/plugin.js'
+import fs from 'fs'
+import YAML from 'yaml'
+
+class Yaml {
+  constructor () {}
+  
+//读
+getread(path) {
+ try {
+    var file = fs.readFileSync(path, 'utf8');
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+  //转换
+  return YAML.parse(file);
+}
+
+getwrite(path,data) {
+  try {
+    //转换
+    let 数据 = YAML.stringify(data);
+    fs.writeFileSync(path, 数据, 'utf8');
+    return true
+  } catch (e) {
+    //错误处理
+    console.log(e);
+    return false
+  }
+}
+  
+  
+  
+}
+  
+  
+export default new Yaml()
