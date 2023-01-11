3682756cd1364fe2f3bc99bc2d35cec4a7b2b46b
花生 <11655106+huasheng1999@user.noreply.gitee.com>
2023-01-11 16:31:11 +0000

    不用api了，12行足以
    Signed-off-by: 花生 <11655106+huasheng1999@user.noreply.gitee.com>

---
+++ apps/现在时间.js
@@ -0,0 +1,12 @@
+import plugin from'../../../lib/plugins/plugin.js'
+import fetch from "node-fetch";
+import moment from "moment";
+export class time extends plugin {
+  constructor () {
+    super({ name: '时间',
+            event: 'message',
+            priority: 5000,
+            rule: [ {reg: "^#*几点了$",
+                     fnc: 'time'},
+                  ]})}
+async time(e) {return e.reply(moment(new Date().getTime()).format('现在是'+'HH'+'点'+'mm'+'分'+'ss'+'秒').replace(/是0/g, "是").replace(/点0/g, "点"));}}
