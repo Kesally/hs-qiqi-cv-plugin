c7422a3e6f02951ed536a171670620e3f01ffafe
七七 <12011613+fengyeplugin@user.noreply.gitee.com>
2023-01-13 06:44:38 +0000

    update apps/ai.js.
    Signed-off-by: 七七 <12011613+fengyeplugin@user.noreply.gitee.com>

--- apps/ai.js
+++ apps/ai.js
@@ -33,7 +33,7 @@ export class ai extends plugin {
       /** https://oicqjs.github.io/oicq/#events */
       event: 'message',
       /** 优先级，数字越小等级越高 */
-      priority: 9000,
+      priority: 58888,
       rule: [
         {
           /** 命令正则匹配 */
