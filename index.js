6ded5eea5ea20e2c79fbb1ec6d892da39fc8add1
kesally <2770706493@qq.com>
2023-04-21 15:20:17 +0000

    修改插件导入消息
    Signed-off-by: kesally <2770706493@qq.com>

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ let ret = []
 
 logger.info(chalk.cyan('-----------(∗❛ั∀❛ั∗)✧*。--------------'))
 logger.info(`~\t${chalk.yellow(`欢迎使用枫叶插件${Ver.ver}`)}\t~`)
-logger.info(`~\t${chalk.green('原作者->')}${'  '}${chalk.cyan('hs&七七(3483342229)')}\t`)
+logger.info(`~\t${chalk.green('原作者->')}${'  '}${chalk.cyan('kesally&七七(2770706493)')}\t`)
 logger.info(`~\t${chalk.green('枫叶群')}${'  '}${chalk.underline('779217677')}\t~`)
+logger.info(`~\t${chalk.green('我们的频道')}${'  '}${chalk.underline('https://pd.qq.com/s/13ptnp2ew')}\t~`)
 logger.info(`~\t${chalk.green('ikun群')}${'  '}${chalk.underline('707499227')}\t~`)
 logger.info(chalk.magenta('------------------------------------'))
 
