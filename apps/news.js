f4a594f7e73118c18dd09e00c1f05b504cd09d86
等风来 <dengfenglai547@qq.com>
2024-07-18 18:23:46 +0800

    fix:今日新闻 fix #IADTPO
    Signed-off-by: 等风来 <dengfenglai547@qq.com>

--- apps/news.js
+++ apps/news.js
@@ -4,7 +4,7 @@ export class example extends plugin {
       /** 功能名称 */
       name: "[枫叶]今日新闻",
       /** 功能描述 */
-      dsc: "简单开发示例",
+      dsc: "每日60s读懂世界",
       event: "message",
       /** 优先级，数字越小等级越高 */
       priority: 3399,
@@ -20,18 +20,8 @@ export class example extends plugin {
   }
 
   async news(e) {
-  // e.msg 用户的命令消息
-    console.log("用户命令：", e.msg)
+    let url = "https://api.jun.la/60s.php?format=image"
 
-    // 执行的逻辑功能
-    let url = "https://api.qqsuu.cn/api/dm-60s"
-    let msg = [
-      segment.at(e.user_id),
-      segment.image(url)
-    ]
-    // 发送消息
-    e.reply(msg)
-
-    return true // 返回true 阻挡消息不再往下
+    return e.reply(segment.image(url), true)
   }
 }
