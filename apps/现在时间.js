2f6bedf67573248b7b0584eba3c919f4130892e2
Kesally <admin@kesally.ren>
2025-01-30 01:12:50 +0800

    优化正则匹配

--- apps/现在时间.js
+++ apps/现在时间.js
@@ -7,12 +7,14 @@ export class time extends plugin {
       priority: 5000,
       rule: [
         {
-          reg: "^#几点了$",
+          reg: /^#?(现在时间|现在几点|现在几点了|几点了)$/i,
           fnc: "time"
         }
       ]
     })
   }
 
-  async time(e) { return e.reply(moment(new Date().getTime()).format("现在是" + "HH" + "点" + "mm" + "分" + "ss" + "秒").replace(/是0/g, "是").replace(/点0/g, "点")) }
+  async time(e) { 
+    return e.reply(moment(new Date().getTime()).format("现在是" + "HH" + "点" + "mm" + "分" + "ss" + "秒").replace(/是0/g, "是").replace(/点0/g, "点")) 
+  }
 }
